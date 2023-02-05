b86335b24fa11c1d004f1fb4214b9acbb3aa30b4
lupprl <n7url@yandex.ru>
2023-02-06 01:26:11 +0400

    iter2

--- internal/app/server/server.go
+++ internal/app/server/server.go
@@ -1,13 +1,16 @@
 package server
 
 import (
+	"github.com/lupprl/goshortener/internal/app/handlers"
 	"log"
 	"net/http"
 )
 
 func Run() {
-	log.Println("-~- starting API v0.1 -~-")
+	log.Println("-~- starting -~-")
 	log.Printf("[ ~ ] starting server 127.0.0.1: on port 8080")
 	log.Panic(http.ListenAndServe(":8080", nil))
-	http.HandleFunc("/", Shortener)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		handlers.Shortener()
+	})
 }
